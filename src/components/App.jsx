a7a32a2fc633b4392891b8b1e45b7f7629363151
Red <rachel.ik.87@gmail.com>
2023-10-13 15:01:48 +0800

    Create input field

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,11 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function createNotes(noteItem) {
     return <Note 
+        key={noteItem.Key}
         title={noteItem.title}
         content={noteItem.content}
     />
@@ -14,6 +16,7 @@ function createNotes(noteItem) {
 function App() {
     return <div>
         <Header />
+        <CreateArea />
         {notes.map(createNotes)}
         <Footer />
     </div>
